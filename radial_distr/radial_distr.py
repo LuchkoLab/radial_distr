c4341879bbd20af546c7e0ae3faf54711f26651f
T Luchko <tluchko@csun.edu>
2024-06-01 18:35:12 -0700

    working version using cell lists to calculate distance

--- radial_distr/radial_distr.py
+++ radial_distr/radial_distr.py
@@ -5,6 +5,7 @@ import parmed as pmd
 import pandas as pd
 import gridData
 import numba
+import MDAnalysis
 
 def main():
     args = get_args()    
@@ -21,10 +22,10 @@ def main():
     parm = pmd.load_file(args.p,args.y)
     print("test print", parm.coordinates)
     print(parm.coordinates.shape)
-    rdf,dr = compute_rad_dist_numba(parm.coordinates, g.grid,g.midpoints)
+    rdf,dr = compute_rad_dist_numba(parm.coordinates, g.grid, g.midpoints, args.cutoff)
     df = pd.DataFrame({'sep':np.linspace(dr / 2,len(rdf) * dr - dr / 2, len(rdf)), 'rdf':rdf})
     df = df.fillna(0)
-    #rdf = compute_rad_dist(parm.coordinates, g)
+    # df = compute_rad_dist_python(parm.coordinates, g)
     print(df)
     df.to_csv('rdf.csv', index=False)
 
@@ -33,51 +34,70 @@ def get_args():
     parser.add_argument('--guv',help='guv file expected',required=True)
     parser.add_argument('-p',help='parmtop file',required=True)
     parser.add_argument('-y',help='coordinate file',required=True)
+    parser.add_argument('--cutoff', type=float, help = 'Maximum cutoff in Angstroms. This will be automatically reduced if greater than any of the grid dimensions.')
     args = parser.parse_args()
     return args
-@numba.jit(cache=True,nopython=True, nogil=True,parallel=False,fastmath=True)
-def compute_rad_dist_numba(coordinates, grid, midpoints):
-    nbins = 200
+
+
+
+#@numba.jit(cache=True,nopython=True, nogil=True,parallel=False,fastmath=True)
+#@numba.jit(cache=True,forceobj=True, looplift=False, nogil=False,parallel=False,fastmath=True)
+def compute_rad_dist_numba(coordinates, grid, midpoints, max_cutoff=20):
+    
+    origin = np.array((midpoints[0][0], midpoints[1][0], midpoints[2][0]))
+
+    # make sure the bounding box is at least double the size of the grid
+    box = np.array([midpoints[0][-1]*2, midpoints[1][-1]*2, midpoints[2][-1]*2, 90, 90, 90])
+    
+    # shift the coordinates and grid points to have an origin at (0,0,0)
+    shift = coordinates.copy()
+    shift -= origin
+    for i in range(len(origin)):
+        midpoints[i] -= origin[i]
+    
+    # ensure that the maximum distance fits within the box.  This is require FastNS
     dr = 0.1
+    nbins = int(min(max_cutoff/dr, min(box[:3])) / 2 / dr)
+
+    # create the cell list
+    cell_list = MDAnalysis.lib.nsgrid.FastNS(
+        dr * nbins, shift.astype('float32'), 
+        box,
+        pbc=False)
     
     #create arrays for hist and rdf
     hist = np.zeros((nbins), dtype=np.int64)
     rdf = np.zeros(nbins)
-    print("test hist and rdf arrays:", hist, rdf)
+    # print("test hist and rdf arrays:", hist, rdf)
     
     for ix in numba.prange(grid.shape[0]):
         for iy in range(grid.shape[1]):
-            print(iy)
+            print(ix, iy)
             for iz in range(grid.shape[2]):
                 
-                mdpnt = np.array([midpoints[0][ix], midpoints[1][iy], midpoints[2][iz]])
+                mdpnt = np.array([[midpoints[0][ix], midpoints[1][iy], midpoints[2][iz]]])
                 
                 #print("test xyz:", iz, iy, ix, grid[ix,iy,iz]) #will print g(r) value at the given point
-                mindist = 999999.0
-                for iatom in range(coordinates.shape[0]):
-                    #print("Our coordinates are:", iatom, coordinates[iatom],mdpnt,)
-                    
-                    # match each g(r) value on each gridpoint to each atom and calculate the distance in numpy
-                    dist = np.linalg.norm(mdpnt - coordinates[iatom])
-                    #print("Our distance is:", dist)
-                    #test if this is the min dist
-                    
-                    if dist < mindist:
-                        mindist = dist
-                        #print("the minimum distance is", mindist)
-                        #find the bin number
-                binnum = int(mindist / dr)     
-                #print("The bin number is:", binnum)
-                if nbins > binnum:
-                    rdf[binnum] += grid[ix,iy,iz] #increment by the g(r) values
-                    hist [binnum] += 1 
-            if iy == 2: return
+                result = cell_list.search(mdpnt.astype('float32'))
+                try:
+                    mindist = result.get_pair_distances().min()
+                    binnum = int(mindist / dr)     
+                    #print("The bin number is:", binnum)
+                    if nbins > binnum:
+                        rdf[binnum] += grid[ix,iy,iz] #increment by the g(r) values
+                        hist [binnum] += 1 
+                except ValueError:
+                    # if there are no atoms within the cutoff
+                    # print(ix,iy,iz)
+                    pass
+                
+            # if iy == 2: return
     #normalize
     rdf = rdf / hist
     #df is data frame
     return rdf,dr
     
-def compute_rad_dist(coordinates, grid):
+def compute_rad_dist_python(coordinates, grid):
     nbins = 200
     dr = 0.1
     
@@ -110,7 +130,6 @@ def compute_rad_dist(coordinates, grid):
                 if nbins > binnum:
                     rdf[binnum] += grid.grid[ix,iy,iz] #increment by the g(r) values
                     hist [binnum] += 1 
-        break
     #normalize
     rdf = rdf / hist
     #df is data frame
